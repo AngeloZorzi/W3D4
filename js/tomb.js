9368f7bf062c90c66305e7ca4a01001b80181757
AngeloZorzi <astro.drg666@gmail.com>
2025-02-20 16:04:15 +0100

    tombola 'creata'

---
+++ js/tomb.js
@@ -0,0 +1,27 @@
+const createCells = document.getElementById("tombolaCells");
+const cells = function () {
+  for (let i = 1; i < 91; i++) {
+    newCell = document.createElement("div");
+    newCell.classList.add("cell");
+    newCell.innerText = i;
+    createCells.appendChild(newCell);
+  }
+};
+cells();
+
+//generiamo numeri casuali da 1 a 90
+
+const generateNumb = function () {
+  const random = Math.floor(Math.random() * 100);
+  const selectedNumb = document.getElementsByClassName("cell")[random - 1];
+  if (random > 0) {
+    selectedNumb.classList.add("cellCol");
+  }
+
+  console.log(random);
+};
+//preveniamo default
+const prevDef = document.getElementById("formdef");
+prevDef.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
