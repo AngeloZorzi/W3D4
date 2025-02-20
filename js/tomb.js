09e1a10bdbcd8a53293b47e13642a56828fc6a5e
AngeloZorzi <astro.drg666@gmail.com>
2025-02-20 16:19:59 +0100

    done some changes on css. fixed a js bug

--- js/tomb.js
+++ js/tomb.js
@@ -1,6 +1,6 @@
 const createCells = document.getElementById("tombolaCells");
 const cells = function () {
-  for (let i = 1; i < 91; i++) {
+  for (let i = 1; i <= 90; i++) {
     newCell = document.createElement("div");
     newCell.classList.add("cell");
     newCell.innerText = i;
@@ -12,7 +12,7 @@ cells();
 //generiamo numeri casuali da 1 a 90
 
 const generateNumb = function () {
-  const random = Math.floor(Math.random() * 100);
+  const random = Math.floor(Math.random() * 90) + 1;
   const selectedNumb = document.getElementsByClassName("cell")[random - 1];
   if (random > 0) {
     selectedNumb.classList.add("cellCol");
